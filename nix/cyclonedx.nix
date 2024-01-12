483abc20d1b9021eb7737afb8253e1dc6beeca82
Erin van der Veen <erin.vanderveen@tweag.io>
2024-01-12 15:57:21 +0100

    Package the cyclonedx-cli tool for testing purpose
    TODO: Add to CI in some way.
    Especially the cyclonedx validate --input-file <PATH> is a very good one. This one validates the provided json file against the cyclonedx spec, and is how I caught multiple non-conformities.

---
+++ nix/cyclonedx.nix
@@ -0,0 +1,28 @@
+{ fetchFromGitHub
+, buildDotnetModule
+
+, lib
+}:
+
+buildDotnetModule rec {
+  pname = "cyclonedx-cli";
+  version = "0.25.0";
+
+  src = fetchFromGitHub {
+    owner = "CycloneDX";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-kAMSdUMr/NhsbMBViFJQlzgUNnxWgi/CLb3CW9OpWFo=";
+  };
+
+  projectFile = "cyclonedx-cli.sln";
+
+  nugetDeps = ./deps.nix;
+
+  meta = with lib;
+    {
+      homepage = "https://github.com/CycloneDX/cyclonedx-cli";
+      description = "tool for SBOM analysis, merging, diffs and format conversions";
+      license = licenses.asl20;
+    };
+}
