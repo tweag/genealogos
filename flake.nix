e3f141f9c3fa04d325eba2b892f1a3e20f3537eb
Erin van der Veen <erin.vanderveen@tweag.io>
2024-03-25 13:39:25 +0100

    chore: restructure our Nix packaging a little

--- flake.nix
+++ flake.nix
@@ -22,56 +22,7 @@
         nixtract-cli = nixtract.defaultPackage.${system};
       in
       rec {
-        packages = rec {
-          default = genealogos;
-          genealogos = naersk-lib.buildPackage rec {
-            name = "genealogos";
-            src = ./.;
-            doCheck = true;
-
-            cargoBuildOptions = x: x ++ [ "--package" name ];
-
-            # Setting this feature flag to disable tests that require recursive nix/an internet connection
-            cargoTestOptions = x: x ++ [ "--package" name "--features" "nix" ];
-
-            # Genealogos uses the reqwest crate to query for narinfo on the substituters.
-            # reqwest depends on openssl.
-            nativeBuildInputs = with pkgs;
-              [ pkg-config ];
-            buildInputs = with pkgs; [ openssl ];
-          };
-          genealogos-cli = naersk-lib.buildPackage rec {
-            name = "genealogos-cli";
-            src = ./.;
-            doCheck = true;
-
-            cargoBuildOptions = x: x ++ [ "--package" name ];
-
-            # Setting this feature flag to disable tests that require recursive nix/an internet connection
-            cargoTestOptions = x: x ++ [ "--package" name "--features" "nix" ];
-
-            # Genealogos uses the reqwest crate to query for narinfo on the substituters.
-            # reqwest depends on openssl.
-            nativeBuildInputs = with pkgs;
-              [ pkg-config ];
-            buildInputs = with pkgs; [ openssl ];
-          };
-          update-fixture-output-files = pkgs.writeShellApplication {
-            name = "update-fixture-output-files";
-            runtimeInputs = [ (genealogos-cli.overrideAttrs (_: { doCheck = false; })) pkgs.jq ];
-            text = builtins.readFile ./scripts/update-fixture-output-files.sh;
-          };
-          update-fixture-input-files = pkgs.writeShellApplication {
-            name = "update-fixture-input-files";
-            runtimeInputs = [ nixtract-cli ];
-            text = builtins.readFile ./scripts/update-fixture-input-files.sh;
-          };
-          verify-fixture-files = pkgs.writeShellApplication {
-            name = "verify-fixture-files";
-            runtimeInputs = [ cyclonedx ];
-            text = builtins.readFile ./scripts/verify-fixture-files.sh;
-          };
-        };
+        packages = import ./nix/packages.nix { inherit pkgs naersk-lib cyclonedx nixtract-cli; };
         devShells = {
           default =
             pkgs.mkShell {
