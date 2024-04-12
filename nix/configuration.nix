ac83618bdc67f49fd8d04bf134fdfaceec7311b2
Erin van der Veen <erin.vanderveen@tweag.io>
2024-04-12 11:51:30 +0200

    chore: port-forward in vm to remove gnome and firefox

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,23 +1,23 @@
-{ config, lib, pkgs, ... }:
+{ ... }:
 
 {
   virtualisation.vmVariant = {
     virtualisation = {
       memorySize = 2048; # Use 2048MiB memory.
       cores = 3;
+      forwardPorts = [
+        {
+          from = "host";
+          guest.port = 8000;
+          host.port = 8000;
+        }
+      ];
     };
   };
-
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   users.users.alice = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      firefox
-    ];
     password = "genealogos";
   };
 
